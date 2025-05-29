b28976f233ccd4e78dc28515d90e64a18ab23d69
melinaloza <mlozaflores@frba.utn.edu.ar>
2025-05-29 15:52:37 -0300

    ahora se modela origen como interfaz y sus distintos tipos

--- servicioAgregador/src/main/java/ar/edu/utn/frba/dds/servicioAgregador/model/entities/Origen.java
+++ servicioAgregador/src/main/java/ar/edu/utn/frba/dds/servicioAgregador/model/entities/Origen.java
@@ -1,7 +1,7 @@
 package ar.edu.utn.frba.dds.servicioAgregador.model.entities;
 
-public enum Origen {
-  CARGAMANUAL,
-  PORCONTRIBUYENTE,
-  DATASET
+public interface Origen {
+    boolean permiteSolicitud();
+
+
 }
