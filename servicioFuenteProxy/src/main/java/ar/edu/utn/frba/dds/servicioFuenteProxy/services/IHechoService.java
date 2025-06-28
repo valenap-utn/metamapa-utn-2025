1ec5a392b5ddd1318c61dafa4303592235eda1e8
bcastoldi <bcastoldi@frba.utn.edu.ar>
2025-06-28 20:28:56 -0300

    Cambio en interfaz

--- servicioFuenteProxy/src/main/java/ar/edu/utn/frba/dds/servicioFuenteProxy/services/IHechoService.java
+++ servicioFuenteProxy/src/main/java/ar/edu/utn/frba/dds/servicioFuenteProxy/services/IHechoService.java
@@ -3,9 +3,18 @@ package ar.edu.utn.frba.dds.servicioFuenteProxy.services;
 import ar.edu.utn.frba.dds.servicioFuenteProxy.clients.dtos.input.HechoInputDTO;
 import ar.edu.utn.frba.dds.servicioFuenteProxy.clients.dtos.output.HechoOutputDTO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IHechoService {
-    List<HechoOutputDTO> getHechosExternos(); // <-- se encarga de hacer la llamada al endpoint de login y devolver un Mono con el token
+    List<HechoOutputDTO> getHechosExternos(
+            String categoria,
+            Double latitud,
+            Double longitud,
+            LocalDateTime fechaReporteDesde,
+            LocalDateTime fechaReporteHasta,
+            LocalDateTime fechaAcontecimientoDesde,
+            LocalDateTime fechaAcontecimientoHasta
+    ); // <-- se encarga de hacer la llamada al endpoint de login y devolver un Mono con el token
 
 }
