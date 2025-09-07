04afe6f76802b08040588fca8ccba0c3eeb3293b
melinaloza <mlozaflores@frba.utn.edu.ar>
2025-09-07 13:31:53 -0300

    agrego normalizacion compararDirecciones

--- servicioAgregador/src/main/java/ar/edu/utn/frba/dds/servicioAgregador/model/entities/comparaciones/CompararDirecciones.java
+++ servicioAgregador/src/main/java/ar/edu/utn/frba/dds/servicioAgregador/model/entities/comparaciones/CompararDirecciones.java
@@ -2,11 +2,28 @@ package ar.edu.utn.frba.dds.servicioAgregador.model.entities.comparaciones;
 
 import ar.edu.utn.frba.dds.servicioAgregador.model.entities.Documento;
 import ar.edu.utn.frba.dds.servicioAgregador.model.entities.Hecho;
+import ar.edu.utn.frba.dds.servicioAgregador.model.entities.Direccion;
 
 public class CompararDirecciones extends ComparacionHechos{
   @Override
   protected Documento elementoAComparar(Hecho hechoExterno) {
-    //TODO
-    return Documento.ofStringSinNormalizar("");//hechoExterno.getDireccion());
+
+    Direccion direccion = hechoExterno.getDireccion();
+    if (direccion == null) {
+      return Documento.ofStringSinNormalizar("");
+    }
+    String direccionStr = String.join(" ",
+            normalizarCampo(direccion.getCiudad()),
+            normalizarCampo(direccion.getLocalidad()),
+            normalizarCampo(direccion.getMunicipio()),
+            normalizarCampo(direccion.getProvincia())
+    );
+
+    return Documento.ofStringSinNormalizar(direccionStr.trim());
   }
+
+    private String normalizarCampo(Object value) {
+      return value == null ? "" : value.toString().toLowerCase().trim();
+  }
+
 }
