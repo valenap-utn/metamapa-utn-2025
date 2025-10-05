23af6cd9e527742dbed793122a86c8e7c07d3229
mgorino <mgorino@frba.utn.edu.ar>
2025-10-05 01:41:37 -0300

    Agrego solo el nuevo repo creado

---
+++ servicioUsuario/src/main/java/ar/edu/utn/frba/dds/servicioUsuario/ServicioUsuarioApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.servicioUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUsuarioApplication.class, args);
+	}
+
+}
---
+++ servicioUsuario/src/test/java/ar/edu/utn/frba/dds/servicioUsuario/ServicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.servicioUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
